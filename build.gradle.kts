9293bce7997ed84a95658dc6a8a84ab7ff18233c
vtung914 <vtung914@gmail.com>
2026-06-24 00:33:05 +0700

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// File cấu hình Gradle dùng cho thư mục gốc của dự án Android
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
